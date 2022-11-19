af10329ce15d8a63ebaae11c15ceeb71f36e2800
Jhon Useche <jhon.useche@telus.com>
2022-11-18 18:59:43 -0700

    Implemented Sum From a to b Recursively

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+// Sum all the values from fromN up to toN
+
+const sum = function(fromN, toN) {
+  if (fromN === toN) {
+    return toN;
+  }
+  return fromN + sum(fromN + 1, toN);
+};
+
+sum(1, 1);
 
 module.exports = sum;
