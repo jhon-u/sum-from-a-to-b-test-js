2c4f01f97302b3ecce8cbfe29b3587e4f609ffd0
Jhon Useche <jhon.useche@telus.com>
2022-12-02 08:51:46 -0700

    Commited

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 // Sum all the values from fromN up to toN
-
 const sum = function(fromN, toN) {
   if (fromN === toN) {
     return toN;
@@ -8,6 +7,11 @@ const sum = function(fromN, toN) {
   return fromN + sum(fromN + 1, toN);
 };
 
+// TEST CODE
 sum(1, 1);
+sum(0, 0);
+sum(5, 5);
+sum(0, 1);
+sum(3, 7);
 
 module.exports = sum;
